6947618c6496c5bbe3e96fde4e7fa6a56c173a74
ClaudiuDivoiu <divoiuclaudiu@gmail.com>
2025-02-01 00:26:57 +0200

    Remove unneeded logger.js file

---
+++ backend/src/config/logger.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+
+const logLevel = process.env.LOG_LEVEL || 'info';
+
+const logger = winston.createLogger({
+    level: logLevel,
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.Console({
+            format: winston.format.simple(),
+        }),
+    ],
+});
+
+module.exports = logger;
